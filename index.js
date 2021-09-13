a0af1a66ebac536cdfd5b15d175b2480c8e1a0e5
Park Kyeong Pyo <as8798as@gmail.com>
2021-09-13 18:03:37 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://parky:qkrrudvy7@expressapp.qs7nb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 즐기자 인생뭐있냐 한번사는거')
+}) 
+
+app.post('/register', (req,res) => {
+  
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
