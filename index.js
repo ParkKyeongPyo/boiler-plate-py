7d7f2405672f24e5eb5643202954e50ae7faae06
Park Kyeong Pyo <as8798as@gmail.com>
2021-09-18 16:03:11 +0900

    로그아웃 기능까지 구현

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/register', (req,res) => {
   
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다.
-  const user = new User(req.body)
+  const user = new User(req.body) //왜 이런 코드를 넣어야하지? json내용을 객체에 담아야 해서?
   user.save((err, doc) => {
     if (err) return res.json({ success: false, err})
     return res.status(200).json({
