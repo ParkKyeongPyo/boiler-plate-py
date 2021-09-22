a5997bb23e5bb1e8caad702f584365caa5860214
Park Kyeong Pyo <as8798as@gmail.com>
2021-09-22 17:56:36 +0900

    redux와 component공부중

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './components/views/LandingPage/LandingPage';
 import reportWebVitals from './reportWebVitals';
+
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
