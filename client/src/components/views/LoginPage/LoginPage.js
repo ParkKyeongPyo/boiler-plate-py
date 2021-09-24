7adf7c2346630881cf30785b2bd1f914a6a2b9b3
Park Kyeong Pyo <as8798as@gmail.com>
2021-09-24 18:12:17 +0900

    로그인 페이지 만드는 중

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 function LoginPage() {
     return (
-        <div>
-            
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            <form style={{ display: 'flex', flexDirection: 'column'}}>
+                <label>Email</label>
+                <input type="email" value onChange />
+                <lable>Password</lable>
+                <input type="password" value onChange />
+
+                <br/>
+
+                <button>
+                    Login
+                </button>
+
+
+
+            </form>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/views/LandingPage/LandingPage';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import 'antd/dist/antd.css';
 import {applyMiddleware, createStore} from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers'; //rootReducer로 받아야하는 것 아닌가? index.js 리듀서 파일 자체를 말하는 건가?
+
 
 const createStorewitheMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 
 ReactDOM.render(
   <Provider
-    store={createStorewitheMiddleware()}
+    store={createStorewitheMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__&& 
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
   >
     <React.StrictMode>
       <App />
